f501702a88bfa2bdc379eeeb7240fd1aeed10d4f
Vu Thien Kha <khavt@fsoft.com>
2020-07-14 21:35:19 +0700

    Story-001: Project initial
    - Add package.json
    - Add webpack.config.js
    - Add base React app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container, Button } from 'semantic-ui-react';
+
+const App = () => (
+	<Container>
+		<h1>React Dashboard</h1>
+		<Button>Enter Dashboard</Button>
+	</Container>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,110 @@
+// Environments
+const PROD_ENV = 'production';
+
+// Plugins
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+// Config
+const path = require('path');
+
+module.exports = (env, argv) => {
+	return ({
+		entry: {
+			app: './src/index.js',
+		},
+		output: {
+			path: path.resolve(__dirname, 'dist/'),
+			filename: '[name].[contenthash:8].js'
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader'
+					}
+				},
+				{
+					test: /\.html$/,
+					use: {
+						loader: 'html-loader'
+					}
+				},
+				{
+					test: /\.less$/,
+					use: [
+						{
+							loader: !(argv.mode === PROD_ENV) ? 'style-loader' : MiniCssExtractPlugin.loader // creates style nodes from JS strings
+						},
+						{
+							loader: 'css-loader', // translates CSS into CommonJS
+							options: {
+								modules: { // enable css modules
+									auto: true,
+									localIdentName: "[name]_[local]__[hash:base64:5]"
+								},
+							}
+						},
+						{
+							loader: 'less-loader' // compiles LESS to CSS
+						}
+					]
+				},
+				{
+					test: /\.css$/,
+					use: [
+						{
+							loader: !(argv.mode === PROD_ENV) ? 'style-loader' : MiniCssExtractPlugin.loader // creates style nodes from JS strings
+						},
+						{
+							loader: 'css-loader' // translates CSS into CommonJS
+						}
+					]
+				},
+				{
+					test: /\.(png|svg|jpg|gif)$/,
+					use: [
+						'url-loader',
+					],
+				},
+				{
+					test: /\.(woff|woff2|eot|ttf|otf)$/,
+					use: [
+						'url-loader',
+					],
+				},
+			]
+		},
+		resolve: {extensions: ["*", ".js", ".jsx"]},
+		devServer: {
+			host: '0.0.0.0',
+			port: 3000
+		},
+		plugins: [
+			new HTMLWebpackPlugin({
+				template: './src/index.html',
+				filename: './index.html',
+				inject: 'body'
+			}),
+			new MiniCssExtractPlugin({
+				filename: '[name].[contenthash:8].css'
+			})
+		],
+		optimization: {
+			runtimeChunk: 'single',
+			splitChunks: {
+				cacheGroups: {
+					commons: {
+						test: /[\\/]node_modules[\\/]/,
+						name: 'vendors',
+						chunks: 'all',
+						priority: 1
+					}
+				}
+
+			}
+		}
+	});
+};
