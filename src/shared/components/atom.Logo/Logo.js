3d3de42582228113249844a3f7b4638bfb2d548c
Vu Thien Kha <khavt@fsoft.com>
2020-07-17 02:48:17 +0700

    Story-004: Apply main layout structure
    - Add MainHeader
    - Add MainNavigation
    - Add MasterLayout
    - Implement deviceDetector

---
+++ src/shared/components/atom.Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Icon } from 'semantic-ui-react';
+
+
+const Logo = () => (
+	<>
+		<Icon name={'lightbulb'} />
+		<span>Code2School</span>
+	</>
+);
+
+export default Logo;
