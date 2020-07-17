44ebb818dc2e8ae06d91cdb6f3f1f49619ef69d8
Vu Thien Kha <khavt@fsoft.com>
2020-07-17 13:57:27 +0700

    Story-003: Story-003: Implement node server
    Add json server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const jsonServer = require('json-server');
+const router = jsonServer.router('data.json');
 
 const port = process.env.PORT || 3001;
 const server = express();
@@ -10,6 +12,8 @@ server.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
+server.use(router);
+
 server.listen(port, () => {
 	console.log(`JSON Server is running with port ${port}`)
-})
+});
